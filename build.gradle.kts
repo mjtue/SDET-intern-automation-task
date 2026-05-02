ba759032cd6f0b080dfacebadb6c8426043ab5c3
mjtue <m.j.jakubiec@student.tue.nl>
2026-05-02 17:45:45 +0200

    dependencies and repositories adde

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,24 @@
 plugins {
-    kotlin("jvm") version "2.3.20"
-}
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
+    kotlin("jvm") version "2.0.21"
 }
 
 kotlin {
     jvmToolchain(21)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+    maven("https://www.jetbrains.com/intellij-repository/releases")
+    maven("https://www.jetbrains.com/intellij-repository/snapshots")
+}
+
+dependencies {
+    testImplementation("com.jetbrains.intellij.tools:ide-starter-junit5:+")
+    testImplementation("com.jetbrains.intellij.tools:ide-starter-driver:+")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }
 
 tasks.test {
